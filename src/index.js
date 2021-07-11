abedb49f872c24032171284602843615313719d4
adrianve <adrianve@pm.me>
2021-07-11 12:37:29 -0500

    Initial commit
    project already complete with graphql server, schema, and prisma db and context

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { ApolloServer } = require('apollo-server')
+const { PrismaClient } = require('@prisma/client')
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: async (parent, args, context, info) => {
+            return context.prisma.link.findMany()
+        },
+    },
+    Mutation: {
+        post: (parent, args, context) => {
+            const newLink = context.prisma.link.create({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            })
+            return newLink
+        },
+    },
+}
+
+const fs = require('fs')
+const path = require('path')
+const prisma = new PrismaClient()
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
+    resolvers,
+    context: {
+        prisma,
+    }
+})
+
+server
+    .listen()
+    .then(({ url }) =>
+        console.log(`Server is running on ${url}`)
+    )
