f9c34dc076e5868a9514665986f41722e8ff9ece
adrianve <adrianve@pm.me>
2021-07-12 19:58:50 -0500

    feat: subscriptions
    add ability to subscribe to "Link created" events

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const { PrismaClient } = require('@prisma/client')
 
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
+const Subscription = require('./resolvers/Subscription')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const { PubSub } = require('apollo-server')
+
+const pubsub = new PubSub()
 
 const resolvers = {
     Query,
     Mutation,
+    Subscription,
     User,
     Link,
 }
@@ -28,6 +33,7 @@ const server = new ApolloServer({
         return {
             ...req,
             prisma,
+            pubsub,
             userId:
                 req && req.headers.authorization
                     ? getUserId(req)
